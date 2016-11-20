78351e74812b1df0a0c8298327e6ced3463595bf
Vuk Dukic <vuk.dukic@clara.com>
2016-11-20 12:33:06 -0800

    Update EventListener.scala

--- EventListener.scala
+++ EventListener.scala
@@ -1,3 +1,21 @@
 package com.clara.clowncar.listener
 
 import com.clara.clowncar.processor.FilteringProcessor
+import com.clara.kafka.consumer.{KafkaMessageSource, MessageSource, SynchronousPollingProcessor}
+import com.clara.kafka.{KafkaThriftEventDeserializer, KafkaTopics}
+import com.expeditelabs.thrift.events 
+import com.expeditelabs.thrift.EventMessage
+import com.twitter.finagle.stats.{Stat, StatsReceiver}
+
+object PlatformEventListener {
+  /**
+    * creates a kafka message source to be used for listening for loan snapshots
+    *
+    * @param kafkaServerName - comma separated list of kafka server:portal
+    * @param kafkaGroupIdName - kafka group identifier
+    * @param StatsReceiver
+    * @return - new kafka message source identifier
+    */
+    def 
+}
+
