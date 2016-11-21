b0a3e9dee0d8500fa6925f86e18353728f592b4c
Vuk Dukic <vuk.dukic@clara.com>
2016-11-20 22:10:06 -0800

    Update IntegracijaSFDC.scala

--- App/Scala/IntegracijaSFDC.scala
+++ App/Scala/IntegracijaSFDC.scala
@@ -8,3 +8,17 @@ import com.expeditelabs.analytics.S3AccessFactory // http://docs.aws.amazon.com/
 import com.expeditelabs.analytics.services.S3BackedLoanArchiveLibrary
 import com.expeditelabs.db.repositories.DatabaseTables
 import com.expeditelabs.salesforce.PollingSalesforceTester
+import com.expeditelabs.salesforce.client.{NullSalesforceClient, SalesforceClient}
+import com.expeditelabs.server.ExpediteServer
+import com.expeditelabs.util.config.simpleconfig.DynamoDBBackedSimpleConfig
+import com.expeditelabs.util.config.{EnvironmentMatchers, LoggingConfig}
+import com.expeditelabs.util.{ExpediteEnvironment, ExpediteFlags}
+import com.twitter.conversions.storage.intToStorageUnitableWholeNumber
+import com.twitter.loggin.{Level, LoggerFactory}
+import com.twitter.util._
+
+object ClownCarServer extends ExpediteServer with EnvironmentMatchers {
+  import ExpediteFlags._
+
+  
+}
